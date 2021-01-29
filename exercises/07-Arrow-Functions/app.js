1b9abba4ca99e27997b0275b7ad743f638c4b1ba
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-29 23:18:34 +0000

    ejercicio 7

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,37 @@
+// AQUI SE CREA LA FUNCION DE TIPO FLECHA
+const rapid = (str) => {
+    str = str.toLowerCase();
+    //declaracion 
+    let palabraSinVocales = "";
+    //procesamos
+    for(let i=0; i < str.length; i++)
+    {
+        if (!(str[i]=="a" || str[i]=="e" || str[i] == "i" || str[i] == "o" || str[i] == "u" ))
+        {
+            palabraSinVocales += str[i].toUpperCase();
+        }
+    }
+    //salida
+    return palabraSinVocales;
+}
 
-   // From this line up Do not change code below
-   let str = "John";
-   console.log(rapid(str));
+// const rapid = (str)=>{
+//     let newWords = [];
+//     let vowels = ['a','e','i','o','u'];
+//     for (let i = 0; i < str.length; i++) {
+        
+//         if(vowels.includes(str[i])){
+//             console.log(str[i]);
+//         }else{
+//             newWords.push(str[i].toUpperCase());
+//         }
+//     }
+//     return newWords.join('');
+// }
+
+
+//From this line up Do not change code below
+let str = 123435;
+
+console.log(rapid(str));
+
