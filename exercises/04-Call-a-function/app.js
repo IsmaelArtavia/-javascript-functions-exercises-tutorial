0c32963e5395eff5dce6923ee9588d3c396846f4
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-29 21:23:25 +0000

    ejercicio 3 funciones

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -5,6 +5,19 @@ function calculateArea(length,edge)
 // Your code Below this line:
 
 
+//length = 3  // longitud
+//edge = 6    // borde
+
+let squareArea1 = calculateArea(4,4);
+console.log(squareArea1);
+
+let squareArea2 = calculateArea(2,2);
+console.log(squareArea2);
+
+let squareArea3 = calculateArea(5,5);
+console.log(squareArea3);
+
+
 
 
 
